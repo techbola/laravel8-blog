5f9f059e165b97b55f32765742e79373cb58a1ba
Adebola <bfagbowore@gmail.com>
2021-01-02 13:40:27 +0100

    project setup, tailwind setup, authentication implemented

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
+            return back()->with('status', 'Invalid credentials');
+        }
+
+        return redirect()->route('dashboard');
+    }
+}
